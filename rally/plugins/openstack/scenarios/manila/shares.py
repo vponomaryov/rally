05b537a5ec9c7f65f2b774383fea6a467613fe9e
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2015-05-25 19:13:23 +0300

    Add create_and_delete_share scenario for Manila
    Add 'create_and_delete_share' scenario and task samples for various Manila
    share creation configurations.
    
    Change-Id: Idcb795a92392c2886f322a6fb49facb4827b034f

---
+++ rally/plugins/openstack/scenarios/manila/shares.py
@@ -0,0 +1,49 @@
+# Copyright 2015 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from rally.benchmark.scenarios import base
+from rally.benchmark import validation
+from rally import consts
+from rally.plugins.openstack.scenarios.manila import utils as manila_utils
+
+
+class ManilaShares(manila_utils.ManilaScenario):
+    """Benchmark scenarios for Manila shares."""
+
+    @validation.validate_share_proto()
+    @validation.required_clients("manila")
+    @validation.required_services(consts.Service.MANILA)
+    @base.scenario()
+    def create_and_delete_share(self, share_proto, size=1,
+                                min_sleep=0, max_sleep=0, **kwargs):
+        """Create and delete a share.
+
+        Optional 'min_sleep' and 'max_sleep' parameters allow the scenario
+        to simulate a pause between share creation and deletion
+        (of random duration from [min_sleep, max_sleep]).
+
+        :param share_proto: share protocol, valid values are NFS, CIFS,
+            GlusterFS and HDFS
+        :param size: share size in GB, should be greater than 0
+        :param min_sleep: minimum sleep time in seconds (non-negative)
+        :param max_sleep: maximum sleep time in seconds (non-negative)
+        :param kwargs: optional args to create a share
+        """
+        share = self._create_share(
+            share_proto=share_proto,
+            size=size,
+            **kwargs)
+        self.sleep_between(min_sleep, max_sleep)
+        self._delete_share(share)
---
+++ tests/unit/plugins/openstack/scenarios/manila/test_shares.py
@@ -0,0 +1,42 @@
+# Copyright 2015 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import ddt
+import mock
+
+from rally.plugins.openstack.scenarios.manila import shares
+from tests.unit import test
+
+
+@ddt.ddt
+class ManilaSharesTestCase(test.TestCase):
+
+    @ddt.data(
+        {"share_proto": "nfs", "size": 3},
+        {"share_proto": "cifs", "size": 4,
+         "share_network": "foo", "share_type": "bar"},
+    )
+    def test_create_and_delete_share(self, params):
+        fake_share = mock.MagicMock()
+        scenario = shares.ManilaShares()
+        scenario._create_share = mock.MagicMock(return_value=fake_share)
+        scenario.sleep_between = mock.MagicMock()
+        scenario._delete_share = mock.MagicMock()
+
+        scenario.create_and_delete_share(min_sleep=3, max_sleep=4, **params)
+
+        scenario._create_share.assert_called_once_with(**params)
+        scenario.sleep_between.assert_called_once_with(3, 4)
+        scenario._delete_share.assert_called_once_with(fake_share)
