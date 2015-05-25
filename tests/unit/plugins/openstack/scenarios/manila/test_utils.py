b98f5617beb816d9a9d2f7444df9e0d34a83aaa8
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2015-05-25 19:06:43 +0300

    Add basic support for Manila project
    Manila provides management of shared or distributed filesystems.
    
    Add support for Manila project main features:
    - Instantiate Manila python client
    - Create/delete shares functionality
    
    Change-Id: Ie29be6162e788c880aea6816803c9ddea340bcf7

---
+++ tests/unit/plugins/openstack/scenarios/manila/test_utils.py
@@ -0,0 +1,66 @@
+# Copyright 2015 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from rally.plugins.openstack.scenarios.manila import utils
+from tests.unit import test
+
+BM_UTILS = "rally.benchmark.utils."
+MANILA_UTILS = "rally.plugins.openstack.scenarios.manila.utils.ManilaScenario."
+
+
+class ManilaScenarioTestCase(test.TestCase):
+
+    def setUp(self):
+        super(ManilaScenarioTestCase, self).setUp()
+        self.scenario = utils.ManilaScenario()
+
+    @mock.patch("time.sleep")
+    @mock.patch(MANILA_UTILS + "clients")
+    @mock.patch(BM_UTILS + "wait_for")
+    @mock.patch(BM_UTILS + "get_from_manager")
+    @mock.patch(BM_UTILS + "resource_is")
+    def test__create_share(self, mock_resource_is, mock_get_from_manager,
+                           mock_wait_for, mock_clients, mock_sleep):
+        fake_name = "fake_name"
+        fake_share = mock.Mock()
+        mock_clients.return_value.shares.create.return_value = fake_share
+        self.scenario._generate_random_name = mock.Mock(return_value=fake_name)
+
+        self.scenario._create_share("nfs")
+
+        mock_clients.return_value.shares.create.assert_called_once_with(
+            "nfs", 1, name=fake_name)
+
+        mock_wait_for.assert_called_once_with(
+            fake_share, is_ready=mock.ANY, update_resource=mock.ANY,
+            timeout=300, check_interval=3)
+        mock_resource_is.assert_called_once_with("available")
+        mock_get_from_manager.assert_called_once_with()
+        self.assertTrue(mock_sleep.called)
+
+    @mock.patch(BM_UTILS + "get_from_manager")
+    @mock.patch(BM_UTILS + "wait_for_delete")
+    def test__delete_share(self, mock_wait_for_delete, mock_get_from_manager):
+        fake_share = mock.MagicMock()
+
+        self.scenario._delete_share(fake_share)
+
+        fake_share.delete.assert_called_once_with()
+        mock_get_from_manager.assert_called_once_with(("error_deleting", ))
+        mock_wait_for_delete.assert_called_once_with(
+            fake_share, update_resource=mock.ANY, timeout=180,
+            check_interval=2)
