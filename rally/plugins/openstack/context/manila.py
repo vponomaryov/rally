29842f92488aec10b052e77e297ba0d981e3339b
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2015-05-25 19:10:52 +0300

    Add context for Manila
    Add context that will allow us to do following things:
    
    - Create shares w/o share networks (+ security services)
    - Autocreate share networks
    - Reuse share networks from existing cloud
    
    Change-Id: Ibdd55c3a41659b5b0eb634787bad29a9f7b83b55

---
+++ rally/plugins/openstack/context/manila.py
@@ -0,0 +1,248 @@
+# Copyright 2015 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import itertools
+
+from oslo_config import cfg
+
+from rally.benchmark.context import base
+from rally.benchmark import utils as bench_utils
+from rally.common.i18n import _
+from rally.common import log
+from rally.common import utils
+from rally import consts
+from rally import exceptions
+from rally import osclients
+from rally.plugins.openstack.scenarios.manila import utils as manila_utils
+
+CONF = cfg.CONF
+LOG = log.getLogger(__name__)
+
+
+@base.context(name="manila", order=450)
+class Manila(base.Context):
+    """This context creates resources specific for Manila project."""
+    CONFIG_SCHEMA = {
+        "type": "object",
+        "$schema": consts.JSON_SCHEMA,
+        "properties": {
+            # NOTE(vponomaryov): specifies whether manila should use
+            # share networks for share creation or not.
+            "use_share_networks": {"type": "boolean"},
+
+            # NOTE(vponomaryov): if context arg 'share_networks' has values
+            # then they will be used else share networks will be autocreated -
+            # one for each tenant network. If networks do not exist then will
+            # be created one share network for each tenant without network
+            # data. This context arg will be used only when context arg
+            # "use_share_networks" is set to True and context 'existing_users'
+            # is not empty, considering usage of existing users.
+            # Expected value is list of dicts where tenant Name or ID is key
+            # and list of share_network Names or IDs is value. Example:
+            # share_networks = [
+            #     {"tenant_1_name_or_id": ["share_network_1_name_or_id",
+            #                              "share_network_2_name_or_id"]},
+            #     {"tenant_2_name_or_id": ["share_network_3_name_or_id"]}
+            # ]
+            # Also, make sure that all 'existing users' in appropriate
+            # registered deployment have share networks if its usage is
+            # enbaled, else Rally will randomly take users that does not
+            # satisfy criteria.
+            "share_networks": {"type": "object"},
+
+            # NOTE(vponomaryov): following settings will be used only if share
+            # networks are used and autocreated.
+            "use_security_services": {"type": "boolean"},
+
+            # NOTE(vponomaryov): context arg 'security_services' is expected
+            # to be list of dicts with data for creation of security services.
+            # Example:
+            # security_services = [
+            #     {'type': 'LDAP', 'dns_ip': 'foo_ip', 'server': 'bar_ip',
+            #      'domain': 'quuz_domain', 'user': 'ololo',
+            #      'password': 'fake_password', 'name': 'optional_name'}
+            # ]
+            # Where 'type' is required key and should have one of following
+            # values: 'active_directory', 'kerberos' or 'ldap'.
+            "security_services": {"type": "array"},
+        },
+        "additionalProperties": False
+    }
+    DEFAULT_CONFIG = {
+        "use_share_networks": True,
+        "share_networks": [],
+        "use_security_services": False,
+        "security_services": [],
+    }
+
+    def _setup_for_existing_users(self):
+        if (self.config["use_share_networks"] and
+                not self.config["share_networks"]):
+            msg = _("Usage of share networks was enabled but for deployment "
+                    "with existing users share networks also should be "
+                    "specified via arg 'share_networks'")
+            raise exceptions.ContextSetupFailure(
+                ctx_name=self.get_name(), msg=msg)
+        for tenant_name_or_id, share_networks in self.config[
+                "share_networks"].items():
+            # Verify project existence
+            for tenant in self.context["tenants"].values():
+                if tenant_name_or_id in (tenant["id"], tenant["name"]):
+                    tenant_id = tenant["id"]
+                    endpoint = None
+                    for user in self.context["users"]:
+                        if user["tenant_id"] == tenant_id:
+                            endpoint = user["endpoint"]
+                            break
+                    break
+            else:
+                msg = _("Provided tenant Name or ID '%s' was not found in "
+                        "existing tenants.") % tenant_name_or_id
+                raise exceptions.ContextSetupFailure(
+                    ctx_name=self.get_name(), msg=msg)
+            self.context["tenants"][tenant_id]["share_networks"] = []
+
+            clients = osclients.Clients(endpoint)
+            manila = manila_utils.ManilaScenario(clients=clients)
+            existing_sns = manila._list_share_networks(
+                detailed=False, search_opts={"project_id": tenant_id})
+
+            for sn_name_or_id in share_networks:
+                # Verify share network existence
+                for sn in existing_sns:
+                    if sn_name_or_id in (sn.id, sn.name):
+                        break
+                else:
+                    msg = _("Specified share network '%(sn)s' does not "
+                            "exist for tenant '%(t)s'") % {
+                                "sn": sn_name_or_id, "t": tenant_id}
+                    raise exceptions.ContextSetupFailure(
+                        ctx_name=self.get_name(), msg=msg)
+
+                # Set share network for project
+                self.context["tenants"][tenant_id][
+                    "share_networks"].append(sn)
+
+            # Add generator over share networks for each project
+            self.context["tenants"][tenant_id]["sn_iterator"] = (
+                itertools.cycle(
+                    self.context["tenants"][tenant_id]["share_networks"]))
+
+        # Set flag that says we will not delete/cleanup share networks
+        self.context["manila_delete_share_networks"] = False
+
+    def _setup_for_autocreated_users(self):
+        # Create share network for each network of tenant
+        for user, tenant_id in (utils.iterate_per_tenants(
+                self.context.get("users", []))):
+            networks = self.context["tenants"][tenant_id].get("networks")
+            clients = osclients.Clients(user["endpoint"])
+            manila = manila_utils.ManilaScenario(clients=clients)
+            self.context["tenants"][tenant_id]["share_networks"] = []
+            self.context["tenants"][tenant_id]["security_services"] = []
+            data = {"name": manila._generate_random_name(
+                prefix="rally_manila_share_network_")}
+
+            def _setup_share_network(tenant_id, data):
+                share_network = manila._create_share_network(**data)
+                self.context["tenants"][tenant_id][
+                    "share_networks"].append(share_network)
+                if self.config["use_security_services"]:
+                    for ss in self.config["security_services"]:
+                        security_service = manila._create_security_service(
+                            **ss)
+                        self.context["tenants"][tenant_id][
+                            "security_services"].append(security_service)
+                        manila._add_security_service_to_share_network(
+                            share_network.id, security_service.id)
+
+            if networks:
+                for network in networks:
+                    if network.get("cidr"):
+                        data["nova_net_id"] = network["id"]
+                    elif network.get("subnets"):
+                        data["neutron_net_id"] = network["id"]
+                        data["neutron_subnet_id"] = network["subnets"][0]
+                    else:
+                        LOG.warning(_(
+                            "Can not determine network service provider. "
+                            "Share network will have no data."))
+                    _setup_share_network(tenant_id, data)
+            else:
+                _setup_share_network(tenant_id, data)
+            # NOTE(vponomaryov): create one infinite iterator over
+            # share networks per tenant. It allows us to balance our shares
+            # among share networks.
+            self.context["tenants"][tenant_id]["sn_iterator"] = (
+                itertools.cycle(
+                    self.context["tenants"][tenant_id]["share_networks"]))
+
+    @utils.log_task_wrapper(LOG.info, _("Enter context: `manila`"))
+    def setup(self):
+        if not self.config["use_share_networks"]:
+            return
+        elif self.context["config"].get("existing_users"):
+            self._setup_for_existing_users()
+        else:
+            self._setup_for_autocreated_users()
+
+    @utils.log_task_wrapper(LOG.info, _("Exit context: `manila`"))
+    def cleanup(self):
+        if not self.context.get("manila_delete_share_networks", True):
+            # NOTE(vponomaryov): assume that share networks were not created
+            # by test run
+            return
+        for user, tenant_id in (utils.iterate_per_tenants(
+                self.context.get("users", []))):
+            clients = osclients.Clients(user["endpoint"])
+            manila = manila_utils.ManilaScenario(clients=clients)
+            objects = [
+                {"list": self.context["tenants"][tenant_id][
+                    "share_networks"] or [],
+                 "name": "share network",
+                 "delete_func": manila._delete_share_network},
+                {"list": self.context["tenants"][tenant_id][
+                    "security_services"] or [],
+                 "name": "security service",
+                 "delete_func": manila._delete_security_service},
+            ]
+            for obj_set in objects:
+                for obj in obj_set["list"]:
+                    logger = log.ExceptionLogger(
+                        LOG,
+                        _("Failed to delete %(name)s %(id)s for tenant "
+                          "%(t)s.") % {"id": obj, "t": tenant_id,
+                                       "name": obj_set["name"]})
+                    with logger:
+                        obj_set["delete_func"](obj)
+
+        # NOTE(vponomaryov): Share network deletion schedules deletion of
+        # share servers. So, we should wait for its deletion too to avoid
+        # further failures of network resources release.
+        # Use separate cycle to make share servers be deleted in parallel.
+        for user, tenant_id in (utils.iterate_per_tenants(
+                self.context.get("users", []))):
+            clients = osclients.Clients(self.context["admin"]["endpoint"])
+            manila = manila_utils.ManilaScenario(clients=clients)
+            for sn in self.context["tenants"][tenant_id]["share_networks"]:
+                share_servers = manila._list_share_servers(
+                    search_opts={"share_network": sn.id})
+                for share_server in share_servers:
+                    bench_utils.wait_for_delete(
+                        share_server,
+                        update_resource=bench_utils.get_from_manager(),
+                        timeout=CONF.benchmark.manila_share_delete_timeout,
+                        check_interval=(
+                            CONF.benchmark.manila_share_delete_poll_interval))
