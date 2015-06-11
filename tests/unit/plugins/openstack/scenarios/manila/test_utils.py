d516418919ea0a1ca942f328d6a05729fd8e017f
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2015-06-11 17:36:25 +0300

    [Manila] Add scenario for creation and deletion of Manila shares
    List of changes:
    - Added support of 'create' and 'delete' operations for shares.
    - Added benchmark for creating and deleting shares without usage
    of share networks.
    
    Change-Id: I48adb06140f70c5d18139726f5af6d7e1ab93a47

--- tests/unit/plugins/openstack/scenarios/manila/test_utils.py
+++ tests/unit/plugins/openstack/scenarios/manila/test_utils.py
@@ -19,6 +19,7 @@ import mock
 from rally.plugins.openstack.scenarios.manila import utils
 from tests.unit import test
 
+BM_UTILS = "rally.benchmark.utils."
 MANILA_UTILS = "rally.plugins.openstack.scenarios.manila.utils.ManilaScenario."
 
 
@@ -28,6 +29,43 @@ class ManilaScenarioTestCase(test.TestCase):
     def setUp(self):
         super(ManilaScenarioTestCase, self).setUp()
         self.scenario = utils.ManilaScenario()
+
+    @mock.patch("time.sleep")
+    @mock.patch(MANILA_UTILS + "clients")
+    @mock.patch(BM_UTILS + "wait_for")
+    @mock.patch(BM_UTILS + "get_from_manager")
+    @mock.patch(BM_UTILS + "resource_is")
+    def test__create_share(self, mock_resource_is, mock_get_from_manager,
+                           mock_wait_for, mock_clients, mock_sleep):
+        fake_name = "fake_name"
+        fake_share = mock.Mock()
+        mock_clients.return_value.shares.create.return_value = fake_share
+        self.scenario._generate_random_name = mock.Mock(return_value=fake_name)
+
+        self.scenario._create_share("nfs")
+
+        mock_clients.return_value.shares.create.assert_called_once_with(
+            "nfs", 1, name=fake_name)
+
+        mock_wait_for.assert_called_once_with(
+            fake_share, is_ready=mock.ANY, update_resource=mock.ANY,
+            timeout=300, check_interval=3)
+        mock_resource_is.assert_called_once_with("available")
+        mock_get_from_manager.assert_called_once_with()
+        self.assertTrue(mock_sleep.called)
+
+    @mock.patch(BM_UTILS + "get_from_manager")
+    @mock.patch(BM_UTILS + "wait_for_delete")
+    def test__delete_share(self, mock_wait_for_delete, mock_get_from_manager):
+        fake_share = mock.MagicMock()
+
+        self.scenario._delete_share(fake_share)
+
+        fake_share.delete.assert_called_once_with()
+        mock_get_from_manager.assert_called_once_with(("error_deleting", ))
+        mock_wait_for_delete.assert_called_once_with(
+            fake_share, update_resource=mock.ANY, timeout=180,
+            check_interval=2)
 
     @ddt.data(
         {},
